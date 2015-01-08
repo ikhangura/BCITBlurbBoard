c5eea9da6b19f9bbbb0c8913b4783bf6eed6c1d7
Ben Soer <bensoer@MAC-EDAMIA.local>
2015-01-07 17:24:58 -0800

    Initial Commit

---
+++ BCITBlurbBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BCITBlurbBoard
+//
+//  Created by Ben Soer on 2015-01-07.
+//  Copyright (c) 2015 Ben Soer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
