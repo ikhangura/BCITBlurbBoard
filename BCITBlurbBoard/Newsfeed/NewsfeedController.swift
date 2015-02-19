15d845d4d09fcb976826b0190f611ef5cc58e4c3
Matthew Banman <Matt@Matthews-Mac.local>
2015-02-18 19:42:07 -0800

    Working on NewsFeed page.
    Added a prototype cell for the list of news items. Currently working on implementing methods to grab data from API
    and plug it into the template.

--- BCITBlurbBoard/Newsfeed/NewsfeedController.swift
+++ BCITBlurbBoard/Newsfeed/NewsfeedController.swift
@@ -7,11 +7,50 @@
 //
 
 import UIKit
-class NewsfeedController: UIViewController {
+class NewsfeedController: UIViewController, UITableViewDelegate, UITableViewDataSource
+{
+        
+    @IBOutlet
+    var tableView: UITableView!
     
+    let news1: [String] = ["Woop woop!", "This is a summary of everything that might be in this post. How exciting!",
+        "Posted February 16, 2015", "D'Arcy Smith, Faculty of Computing", "12"]
+    let news2: [String] = ["Hubba Bubba: Old News or Retro Cool?", "Gum is making a comeback according to a crack team of researchers at BCIT's Burnaby, BC, Canada campus.","Posted February 16, 2015", "Matthew Banman, CST", "3"]
+    let items: [String[]] = [news1, news2]
+    
+    // table view methods
+    
+    override func numberOfSections() -> Int
+    {
+        return 1
+    }
+        
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int ) -> Int
+    {
+        return self.items.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        var cell : UITableViewCell! = self.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        
+        cell.
+           
+        return UITableViewCell()
+    }
+    
+    func tableView(tableView : UITableView, didSelectRowAtIndexPath indexPath : NSIndexPath)
+    {
+
+    }
+    
+    // end table view methods
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell");
     }
     
     override func didReceiveMemoryWarning() {
