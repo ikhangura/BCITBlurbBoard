674041817ee7dcc1b9f78312d3e666cffaf7b319
bensoer <ikben@shaw.ca>
2015-02-28 15:11:37 -0800

    Added SwiftyJSON common class. Added general login logic

--- BCITBlurbBoard/Login/LoginController.swift
+++ BCITBlurbBoard/Login/LoginController.swift
@@ -7,8 +7,17 @@
 //
 
 import UIKit
+import AlamoFire
 
 class LoginController: UIViewController {
+    
+    @IBOutlet var password: UITextField!
+    @IBOutlet var username: UITextField! //is actualy the email input
+    @IBOutlet var error: UILabel!
+    
+    //user data variables
+    var token:String = "";
+    var type:String = "";
     
     // get instance of HTTPClient
     let httpClient = HTTPClient.sharedInstance;
@@ -22,35 +31,118 @@ class LoginController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //closes keyboard when user clicks the done key
+    @IBAction func textFieldDoneEditing(sender: UITextField){
+        sender.resignFirstResponder();
+    }
+    
+    //closes the keyboard when user taps the background
+    @IBAction func backgroundTap(sender: UIControl){
+        username.resignFirstResponder();
+        password.resignFirstResponder();
+    }
 
     @IBAction func LoginUser(sender: UIButton) {
+        
+        
         println( "User logging in!" )
         
-        //TODO : Add actual verification for user auth  
-        
-        // test the login route!
-        /*
-        var loginInfo = ["username":"bsoer@my.bcit.ca", "password":"password"]
-        httpClient.TestPost("http://api.thunderchicken.ca:8080/api/auth", data : loginInfo);
-        */
-        
-        // test newsfeed route
-        var getResult : NSDictionary? = httpClient.Get("http://api.thunderchicken.ca:8080/api/newsfeed/A00843110/standard/QLTw7tisv8O7ipX6B7Tdxnzv6")
-        
-        println( getResult )
-        
-        let auth = true;
-        if (auth)
-        {
-            // Set user data, such as usertoken
-            // Send device token back to server
-            
-            // Send to Newsfeed page
-            var storyboard = UIStoryboard(name : "NewsfeedStoryboard", bundle: nil);
-            var controller = storyboard.instantiateViewControllerWithIdentifier("newsfeed") as UIViewController;
-            let dstController = controller as NewsfeedController;
-            self.presentViewController(controller, animated: true, completion: nil)
+        //client side validation
+        if(!validateEmail(username.text) || !validatePassword((password.text)) ){
+            error.text = "The Email/Password is Incorrect";
+            return;
         }
+        let validEmail = username.text!;
+        let validPassword = password.text!;
+        
+        
+        
+        //server call
+        var loginInfo:[String:AnyObject] = ["email":validEmail, "password": validPassword];
+
+        
+        Alamofire.request(.POST, "http://api.thunderchicken.ca:8080/api/auth", parameters: loginInfo, encoding: .JSON)
+            .responseJSON{ (_, _, data, _) in
+                
+                println("Response Arrived!");
+            
+                //parse with SwiftlyJSON (located in /Common)
+                let json = JSON(data!);
+
+                if var statuscode = json["statuscode"].int {
+                    if (statuscode == 200){
+                        //proceed user
+                        self.logUserIn(json);
+                    }else{
+                        if var logMessage = json["message"].string{
+                            let message:String = "\(statuscode) : \(logMessage)";
+                            println(message);
+                            self.error.text = "The Email/Password is Incorrect";
+                            return;
+                        }
+                    }
+                }
+                
+                
+            }
+        }
+    
+    private func logUserIn(json:JSON){
+        
+        //set user data, such as user token
+        if let userToken = json["data"]["token"].string {
+            self.token = userToken;
+        }
+        // and usertype
+        if let userType = json["data"]["usertype"].string {
+            self.type = userType;
+        }
+        //send device token back to server
+        if(!sentDeviceToken()){
+            println("Failed to send device token. Some functionality may not work. Re-login to try again");
+        }
+        
+        //send to newsfeed page
+        var storyboard = UIStoryboard(name : "NewsfeedStoryboard", bundle: nil);
+        var controller = storyboard.instantiateViewControllerWithIdentifier("newsfeed") as UIViewController;
+        let dstController = controller as NewsfeedController;
+        self.presentViewController(controller, animated: true, completion: nil)
+        
+    }
+    
+    private func sentDeviceToken() -> Bool{
+        
+        return false;
+    }
+    
+    private func validateEmail(email:NSString) -> Bool{
+        
+        let uString = email as String;
+        
+        //starts with any letters or numbers
+        //then an @ which after may or may not have my.
+        //then ends with bcit.ca
+        let emailRegex = "^[A-Za-z0-9]*@(my.)?bcit.ca$";
+        
+        if let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegex){
+            return emailTest.evaluateWithObject(email);
+        }
+        
+        return false;
+        
+    }
+    
+    private func validatePassword(password:NSString) -> Bool {
+        
+        let pString = password as String;
+        
+        //check is not empty
+        if(pString.isEmpty){
+            return false;
+        }
+        
+        return true;
     }
 
 }
