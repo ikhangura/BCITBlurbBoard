ebd06f71be2e3fea12d2e669226ffcb4ce6dadc1
Ben Soer <bensoer@users.noreply.github.com>
2015-03-11 19:48:58 -0700

    Revert "News feed in progress"

--- BCITBlurbBoard/Newsfeed/NewsfeedController.swift
+++ BCITBlurbBoard/Newsfeed/NewsfeedController.swift
@@ -7,58 +7,11 @@
 //
 
 import UIKit
-class NewsfeedController: UIViewController, UITableViewDelegate, UITableViewDataSource
-{
-        
-    @IBOutlet var tableView: UITableView!
+class NewsfeedController: UIViewController {
     
-    let newsItem1 : [String] = ["Woop woop!", "This is a summary of everything that might be in this post. How exciting!",
-        "Posted February 16, 2015", "D'Arcy Smith, Faculty of Computing", "12"]
-    let newsItem2 : [String] = ["Hubba Bubba: Old News or Retro Cool?", "Gum is making a comeback according to a crack team of researchers at BCIT's Burnaby, BC, Canada campus.","Posted February 16, 2015", "Matthew Banman, CST", "3"]
-    //let items: [[String]] = [self.newsItem1 , self.newsItem2]
-    
-    let items: [[String]] = [
-        ["Woop woop!", "This is a summary of everything that might be in this post. How exciting!",
-        "Posted February 16, 2015", "D'Arcy Smith, Faculty of Computing", "12"],
-        ["Hubba Bubba: Old News or Retro Cool?", "Gum is making a comeback according to a crack team of researchers at BCIT's Burnaby, BC, Canada campus.","Posted February 16, 2015", "Matthew Banman, CST", "3"]]
-    
-    // table view methods
-    /*
-    override func numberOfSections() -> Int
-    {
-        return 1
-    }
-     */
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int ) -> Int
-    {
-        return self.items.count
-    }
-
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
-    {
-        var cell : NewsItemCell!         = tableView.dequeueReusableCellWithIdentifier("newsItemCell") as NewsItemCell
-        cell.CellTitle.text              = items[indexPath.row][0]
-        cell.MessagePreview.text         = items[indexPath.row][1]
-        cell.Date.text                   = items[indexPath.row][2]
-        cell.Author.text                 = items[indexPath.row][3]
-        cell.CommentNum.text             = items[indexPath.row][4]
-           
-        return cell
-    }
-    
-    func tableView(tableView : UITableView, didSelectRowAtIndexPath indexPath : NSIndexPath)
-    {
-        // do some stuff here
-    }
-    
-    // end table view methods
-        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "newsItemCell");
     }
     
     override func didReceiveMemoryWarning() {
